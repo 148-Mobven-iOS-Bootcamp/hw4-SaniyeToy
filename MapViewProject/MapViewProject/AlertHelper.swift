30cf09ac907d1f7259eb1c4efafc8e0a787a9158
SaniyeToy <75203610+SaniyeToy@users.noreply.github.com>
2022-01-11 11:35:21 +0300

    IOS-3 create alert helper

---
+++ MapViewProject/MapViewProject/AlertHelper.swift
@@ -0,0 +1,17 @@
+//
+//  AlertHelper.swift
+//  MapViewProject
+//
+//  Created by MacOS on 11.01.2022.
+//
+
+import Foundation
+import UIKit
+extension UIViewController {
+  func alert(message: String, title: String = "") {
+    let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    let OKAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+    alertController.addAction(OKAction)
+    self.present(alertController, animated: true, completion: nil)
+  }
+}
