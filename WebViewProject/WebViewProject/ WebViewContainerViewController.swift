73071fb19fa73a9a2bdef32ac823861e28b25ceb
SaniyeToy <75203610+SaniyeToy@users.noreply.github.com>
2022-01-12 11:56:49 +0300

    IOS-9 Completed code review

--- WebViewProject/WebViewProject/ WebViewContainerViewController.swift
+++ WebViewProject/WebViewProject/ WebViewContainerViewController.swift
@@ -9,13 +9,13 @@ import UIKit
 import WebKit
 
 class WebViewContainerViewController: UIViewController {
-
+    
     @IBOutlet weak var webView: WKWebView?
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         configureWebView()
         configureActivityIndicator()
     }
@@ -30,11 +30,11 @@ class WebViewContainerViewController: UIViewController {
         }
         UIApplication.shared.open(url)
     }
-        
+    
     @IBAction func goForWardButton(_ sender: Any) {
         webView?.goForward()
     }
-
+    
     func configureWebView() {
         guard let url = URL(string: HTMLCodeString.googleString.rawValue) else { return }
         let urlRequest = URLRequest(url: url)
@@ -42,30 +42,30 @@ class WebViewContainerViewController: UIViewController {
         preferences.javaScriptCanOpenWindowsAutomatically = false
         let configuration = WKWebViewConfiguration()
         configuration.preferences = preferences
-//      webView.configuration = configuration
+        //      webView.configuration = configuration
         webView?.uiDelegate = self
         webView?.navigationDelegate = self
         webView?.allowsBackForwardNavigationGestures = true
         webView?.addObserver(self,
-                            forKeyPath: #keyPath(WKWebView.isLoading),
-                            options: .new,
-                            context: nil)
-       // webView?.load(urlRequest)
+                             forKeyPath: #keyPath(WKWebView.isLoading),
+                             options: .new,
+                             context: nil)
+        // webView?.load(urlRequest)
         let fontSetting = "<span style=\"font-family: \("PingFangSC-Light");font-size: \(40)\"</span>"
         webView?.loadHTMLString(fontSetting + HTMLCodeString.mobvenHTML.rawValue, baseURL: nil)
     }
-
+    
     func configureActivityIndicator() {
         activityIndicator.style = .large
         activityIndicator.color = .systemMint
         activityIndicator.hidesWhenStopped = true
     }
-
+    
     override func observeValue(forKeyPath keyPath: String?,
                                of object: Any?,
                                change: [NSKeyValueChangeKey : Any]?,
                                context: UnsafeMutableRawPointer?) {
-
+        
         if keyPath == "loading" {
             webView!.isLoading ? activityIndicator.startAnimating() : activityIndicator.stopAnimating()
         }
@@ -74,9 +74,9 @@ class WebViewContainerViewController: UIViewController {
 
 
 extension WebViewContainerViewController: WKNavigationDelegate {
-
+    
 }
 
 extension WebViewContainerViewController: WKUIDelegate {
-
+    
 }
